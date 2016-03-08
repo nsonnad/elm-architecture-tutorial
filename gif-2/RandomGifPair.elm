a7b231e62667877f14e1c8ac52741f8bfd7a612a
Nikhil S <nikhil.sonnad@gmail.com>
2016-03-07 20:13:43 -0500

    add gif-2

---
+++ gif-2/RandomGifPair.elm
@@ -0,0 +1,65 @@
+module RandomGifPair where
+
+import Effects exposing (Effects)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+import RandomGif
+
+
+-- MODEL
+
+type alias Model =
+  { left : RandomGif.Model
+  , right: RandomGif.Model
+  }
+
+
+init : String -> String -> (Model, Effects Action)
+init leftTopic rightTopic =
+  let
+      (left, leftFx) = RandomGif.init leftTopic
+      (right, rightFx) = RandomGif.init rightTopic
+  in
+     ( Model left right
+     , Effects.batch
+        [ Effects.map Left leftFx
+        , Effects.map Right rightFx
+        ]
+     )
+
+
+-- UPDATE
+
+type Action
+  = Left RandomGif.Action
+  | Right RandomGif.Action
+
+update : Action -> Model -> (Model, Effects Action)
+update action model =
+  case action of
+    Left msg ->
+      let
+          (left, fx) = RandomGif.update msg model.left
+      in
+         ( Model left model.right
+         , Effects.map Left fx
+         )
+
+    Right msg ->
+      let
+          (right, fx) = RandomGif.update msg model.right
+      in
+         ( Model model.left right
+         , Effects.map Right fx
+         )
+
+
+-- VIEW
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div [ style [ ("display", "flex") ] ]
+    [ RandomGif.view (Signal.forwardTo address Left) model.left
+    , RandomGif.view (Signal.forwardTo address Right) model.right
+    ]
